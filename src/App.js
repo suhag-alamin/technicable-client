915369b6ab90a0be331b7ec3133b3567bab4a5c5
developer-suhag <developersuhag@gmail.com>
2023-01-02 20:08:59 +0600

    custom color plate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
-// #3A9BDC
-// #050A30
-// #4942CE
-// #F5F8FA
-// #F25F5C
+// https://web.programming-hero.com/api/blog/get-display-blog-list
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold text-cyan-500">hello world</h1>
+      <h1 className="text-3xl font-bold text-technicablePrimary">
+        hello world
+      </h1>
+      <p className="text-technicableSecondary">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        blanditiis odit mollitia perspiciatis ab maiores nam laboriosam libero
+        eaque vero! Ipsam tempora, nisi ea consequatur mollitia soluta tenetur
+        inventore tempore.
+      </p>
     </div>
   );
-}
+};
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      techniblePrimary: "#3A9BDC",
+      technibleSecondary: "#050A30",
+      technibleTeriary: "#4942CE",
+      technibleWhite: "#F5F8FA",
+      technibleRed: "#F25F5C",
+    },
   },
   plugins: [],
 };
